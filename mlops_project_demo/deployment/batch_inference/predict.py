efc9a79499e8d246100991ba97d68c246c2ad235
alex.miller@databricks.com <alex.miller@databricks.com>
2024-06-10 23:46:58 +0000

    update batch_inference and predict functions

--- mlops_project_demo/deployment/batch_inference/predict.py
+++ mlops_project_demo/deployment/batch_inference/predict.py
@@ -1,4 +1,5 @@
 import mlflow
+from databricks.feature_engineering import FeatureEngineeringClient
 from pyspark.sql.functions import struct, lit, to_timestamp
 
 
@@ -9,16 +10,15 @@ def predict_batch(
     Apply the model at the specified URI for batch inference on the table with name input_table_name,
     writing results to the table with name output_table_name
     """
-    mlflow.set_registry_uri("databricks-uc")
-    table = spark_session.table(input_table_name)
-    
-    from databricks.feature_store import FeatureStoreClient
-    
-    fs_client = FeatureStoreClient()
 
-    prediction_df = fs_client.score_batch(
-        model_uri,
-        table
+    mlflow.set_registry_uri("databricks-uc")
+    table = spark_session.table(input_table_name).select('ts', 'purchased', 'destination_id', 'user_id', 'user_latitude', 'user_longitude', 'booking_date')
+    
+    fe_client = FeatureEngineeringClient()
+
+    prediction_df = fe_client.score_batch(
+        model_uri=model_uri,
+        df=table
     )
     output_df = (
         prediction_df.withColumn("prediction", prediction_df["prediction"])
@@ -26,7 +26,7 @@ def predict_batch(
         .withColumn("inference_timestamp", to_timestamp(lit(ts)))
     )
     
-    output_df.display()
+    # output_df.display()
     # Model predictions are written to the Delta table provided as input.
     # Delta is the default format in Databricks Runtime 8.0 and above.
     output_df.write.format("delta").mode("overwrite").saveAsTable(output_table_name)
