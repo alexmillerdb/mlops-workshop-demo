470c7b9cde19a65e551e1ebf6117600c321009f9
alex.miller@databricks.com <alex.miller@databricks.com>
2024-06-18 12:23:13 +0000

    update

--- mlops_project_demo/deployment/model_serving/notebooks/DeployEndpoint.py
+++ mlops_project_demo/deployment/model_serving/notebooks/DeployEndpoint.py
@@ -6,10 +6,6 @@ notebook_path =  '/Workspace/' + os.path.dirname(dbutils.notebook.entry_point.ge
 
 # COMMAND ----------
 
-# %pip install -r ../../../requirements.txt
-
-# COMMAND ----------
-
 # MAGIC %pip install databricks-feature-engineering==0.2.0 databricks-sdk==0.20.0
 
 # COMMAND ----------
